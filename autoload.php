bdd5dd83ce1492fb0a78b484cdf03c6e24f7ceb6
dwcs-code-2425 <dwcs.code.2425@gmail.com>
2025-01-07 20:25:06 +0100

    init commit

---
+++ autoload.php
@@ -0,0 +1,19 @@
+<?php
+const CLASES_FOLDER="clases";
+const  PEOPLE_FOLDER="people";
+const  ITEMS_FOLDER="items";
+
+spl_autoload_register(function ($nombre_clase) {
+
+    //echo "## $nombre_clase<br/>";
+    $dirs= [PEOPLE_FOLDER, ITEMS_FOLDER];
+    foreach ($dirs as $dir) {
+        $ruta = CLASES_FOLDER.DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR.$nombre_clase.'.php';
+        if (file_exists($ruta)){
+            require_once $ruta;
+            //parar de iterar para esa $nombre_clase que ya se ha encontrado
+            return;
+        }
+    }
+    
+});
